7f43900e3a7fcf0918acf674a61aa2341de51b6a
Maozhu Liu <uczlm97@ucl.ac.uk>
2019-03-13 15:08:05 -0400

    Set up the server

--- studentServer.js
+++ studentServer.js
@@ -8,6 +8,12 @@ var app = express();
 var http = require('http');
 var httpServer = http.createServer(app);
 httpServer.listen(4480);
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+extended: true
+}));
+app.use(bodyParser.json());
+
 /*app.get('/',function (req,res) {
 	res.send("hello world from the HTTP server");
 });*/
@@ -36,6 +42,35 @@ app.use(function (req, res, next) {
 	console.log("The file " + filename + " was requested.");
 	next();
 });*/
+
+var fs = require('fs');
+var pg = require('pg');
+var configtext =""+fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
+// now convert the configruation file into the correct format -i.e. a name/value pair array
+var configarray = configtext.split(",");
+var config = {};
+for (var i = 0; i < configarray.length; i++) {
+var split = configarray[i].split(':');
+config[split[0].trim()] = split[1].trim();
+}
+var pool = new pg.Pool(config);
+app.get('/postgistest', function (req,res) {
+	pool.connect(function(err,client,done) {
+		if(err){
+			console.log("not able to get connection "+ err);
+			res.status(400).send(err);
+		}
+		client.query('SELECT name FROM london_poi' ,function(err,result) {
+		done();
+		if(err){
+			console.log(err);
+			res.status(400).send(err);
+		}
+		res.status(200).send(result.rows);
+		});
+	});
+});
+
 
 
 app.use(function(req, res, next) {
