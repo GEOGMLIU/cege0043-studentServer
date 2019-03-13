1b45ae23bdcb1fe4a2c9c5ed1211e575abde53e2
Maozhu Liu <uczlm97@ucl.ac.uk>
2019-03-13 13:47:51 -0400

    modified version that can calls various html file

--- studentServer.js
+++ studentServer.js
@@ -8,8 +8,21 @@ var app = express();
 var http = require('http');
 var httpServer = http.createServer(app);
 httpServer.listen(4480);
-app.get('/',function (req,res) {
+/*app.get('/',function (req,res) {
 	res.send("hello world from the HTTP server");
+});*/
+/*app.get('/test.html', function (req, res) {
+	// run some server-side code
+	console.log('test.html requested');
+	// note that __dirname gives the path to the studentServer.js file
+	res.sendFile(__dirname + '/test.html');
+});*/
+app.get('/:fileName', function (req, res) {
+	// run some server-side code
+	var fileName = req.params.fileName;
+	console.log(fileName + ' requested');
+	// note that __dirname gives the path to the studentServer.js file
+	res.sendFile(__dirname + '/'+ fileName);
 });
 // adding functionality to log the requests
 app.use(function (req, res, next) {
